c351871ce131f16e2b133aa3005ff2e1c89b35c5
Rania Azad <75755469+rania-azad@users.noreply.github.com>
2024-09-14 21:55:47 +0300

    Update data.py

--- src/data.py
+++ src/data.py
@@ -8,31 +8,38 @@ import numpy as np
 from skimage.color import rgb2lab, lab2rgb
 
 SIZE = 256
+# create another dataset
 class ColorizationDataset(Dataset):
     def __init__(self, paths, split='train'):
         if split == 'train':
             self.transforms = transforms.Compose([
-                transforms.Resize((SIZE, SIZE),  Image.BICUBIC),
-                # TODO: A little data augmentation FLIPS?!
+                transforms.Resize((SIZE, SIZE),  Image.BICUBIC), 
+                transforms.RandomHorizontalFlip(), # flip only the images that needs such colorization
+                transforms.ToTensor() # convert to Tensors
+                
             ])
         elif split == 'val':
-            self.transforms = transforms.Resize((SIZE, SIZE),  Image.BICUBIC)
-
+            self.transforms = transforms.Compose([
+              transforms.Resize((SIZE, SIZE),  Image.BICUBIC),
+              transforms.ToTensor() # convert to Tensors
+            ])
         self.split = split
         self.size = SIZE
         self.paths = paths
 
     def __getitem__(self, idx):
         img = Image.open(self.paths[idx]).convert("RGB")
-        # TODO: apply transforms 
-        img = np.array(img)
+        img= self.transforms(img)
+        
+        #shape and flip
+        img =img.permute(1,2,0).numpy()
         img_lab = rgb2lab(img).astype("float32") # Converting RGB to L*a*b
-        # TODO: convert to tensor 
+      
 
-        L = img_lab[[0], ...] / 50. - 1. # Between -1 and 1
-        ab = img_lab[[1, 2], ...] / 110. # Between -1 and 1
+        L = torch.tensor(img_lab[[0], ...] / 50. - 1.) # Between -1 and 1
+        ab = img_lab([[1, 2], ...] / 110.) # Between -1 and 1
 
-        return {'L': L, 'ab': ab}
+        return {'L': L.unsqueeze(0), 'ab': ab}
 
     def __len__(self):
         return len(self.paths)
